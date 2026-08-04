13720f4a07e74ce9b8b38fc931a2e230aa93a37d
avelorsolution-cell <avelorsolution@gmail.com>
2026-08-04 09:14:31 +0300

    Delete js directory

--- js/animation.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  // Lazy loading fallback class toggler for elements using data-src pattern (future images)
-  const lazyImgs = document.querySelectorAll('img[loading="lazy"]');
-  lazyImgs.forEach((img) => {
-    img.addEventListener('load', () => img.classList.add('is-loaded'));
-  });
-})();
